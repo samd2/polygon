449293fdebc277cd0fb705f96f4c8763b6988be4
sdarwin <samuel.d.darwin@gmail.com>
2020-12-21 23:50:43 +0000

    drone

---
+++ .drone/boost.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -ex
+export TRAVIS_BUILD_DIR=$(pwd)
+export TRAVIS_BRANCH=$DRONE_BRANCH
+export TRAVIS_OS_NAME=${DRONE_JOB_OS_NAME:-linux}
+export VCS_COMMIT_ID=$DRONE_COMMIT
+export GIT_COMMIT=$DRONE_COMMIT
+export DRONE_CURRENT_BUILD_DIR=$(pwd)
+export PATH=~/.local/bin:/usr/local/bin:$PATH
+
+echo '==================================> BEFORE_INSTALL'
+
+. .drone/before-install.sh
+
+echo '==================================> INSTALL'
+
+BOOST_BRANCH=develop && [ "$TRAVIS_BRANCH" == "master" ] && BOOST_BRANCH=master || true
+cd ..
+git clone -b $BOOST_BRANCH https://github.com/boostorg/boost.git boost
+cd boost
+git submodule update --init tools/build
+git submodule update --init libs/config
+git submodule update --init tools/boostdep
+mkdir -p libs/polygon
+cp -r $TRAVIS_BUILD_DIR/* libs/polygon
+python tools/boostdep/depinst/depinst.py polygon
+./bootstrap.sh
+./b2 headers
+
+echo '==================================> BEFORE_SCRIPT'
+
+. $DRONE_CURRENT_BUILD_DIR/.drone/before-script.sh
+
+echo '==================================> SCRIPT'
+
+echo "using $TOOLSET : : $COMPILER ;" > ~/user-config.jam
+./b2 -j 3 libs/polygon/test toolset=$TOOLSET cxxstd=$CXXSTD ${VARIANT:+variant=$VARIANT}
+
+echo '==================================> AFTER_SUCCESS'
+
+. $DRONE_CURRENT_BUILD_DIR/.drone/after-success.sh
